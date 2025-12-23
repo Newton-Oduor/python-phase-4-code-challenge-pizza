f236edc5a90b34f25b27e4688964bd7be407a649
Newton Oduor <newtonochieng@gmail.com>
2025-12-23 10:22:13 +0300

    Adds FK column in the class RestaurantPizza

--- server/models.py
+++ server/models.py
@@ -51,6 +51,9 @@ class RestaurantPizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
+    restaurant_id = db.column(db.integer, db.ForeignKey("restaurants.id"), nullable=False)
+    pizza_id = db.column(db.integer, db.ForeignKey("pizzas.id"), nullable=False)
+
     restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
     pizza = db.relationship("Pizza", back_populates="restaurant_pizzas")
 
